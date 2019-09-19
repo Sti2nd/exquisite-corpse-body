1a27bffa14de95642e65077b57cb75a2df452728
Sti2nd <stianahj@gmail.com>
2019-09-19 21:32:56 +0200

    add build folder

---
+++ precache-manifest.08724019c41f93e849ba034ce733437b.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "./static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "./static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "./static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "./static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "2de1e80b19002dcac421",
+    "url": "./static/js/main.2de1e80b.chunk.js"
+  },
+  {
+    "revision": "c524c07fffa52103769f",
+    "url": "./static/js/1.c524c07f.chunk.js"
+  },
+  {
+    "revision": "2de1e80b19002dcac421",
+    "url": "./static/css/main.cbae0ea8.chunk.css"
+  },
+  {
+    "revision": "c524c07fffa52103769f",
+    "url": "./static/css/1.e1f1e808.chunk.css"
+  },
+  {
+    "revision": "ffa6b2303f465d6ccbcc30db77172320",
+    "url": "./index.html"
+  }
+];
